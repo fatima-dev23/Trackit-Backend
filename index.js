8f40a34fc7ebd9adbae2858008c9b1cd98b8af98
fatima-dev <fatimasarwar105@gmail.com>
2025-04-27 22:45:36 +0500

    added star

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
